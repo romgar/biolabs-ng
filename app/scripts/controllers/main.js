10373b96fc58628d834c26395bf8ca9d76e5bd2d
Natsu- <romain.garrigues.cs@gmail.com>
2014-05-14 22:22:14 +0200

    Initial angular project generation from yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('biolabsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
