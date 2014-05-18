ab999106fb2a8a8d269eb68c623870888afb8c02
Natsu- <romain.garrigues.cs@gmail.com>
2014-05-18 21:45:17 +0200

    Adding devserve task

--- Gruntfile.js
+++ Gruntfile.js
@@ -402,6 +402,19 @@ module.exports = function (grunt) {
   });
 
 
+  grunt.registerTask('devserve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'devbuild',
+      'concurrent:server',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
   grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
