a15d6510092f48ac77372787ad5f76326d422336
Natsu- <romain.garrigues.cs@gmail.com>
2014-05-30 18:43:27 +0200

    Simulating service to get labs infos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('biolabsApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, GeoJsonService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    GeoJsonService.getData()
+    .then(function(result) {
+        $scope.markers = result;
+    });
+});
---
+++ app/scripts/data/geoJsonService.js
@@ -0,0 +1,29 @@
+angular.module('biolabsApp')
+  .service('GeoJsonService', ['$q', function($q) {
+
+    this.getData = function() {
+
+        var deferred = $q.defer();
+
+        var exampleData = [
+            {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: true,
+                draggable: false
+            },
+            {
+                lat: 59.91,
+                lng: 40.75,
+                message: "I want to travel here too",
+                draggable: true
+            },        
+        ];
+
+        deferred.resolve(exampleData);
+
+        return deferred.promise;
+    };
+
+  }]);
