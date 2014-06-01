236fc4a282465ac87cf17c9b09be5ac7dc1dbfc8
Natsu- <romain.garrigues.cs@gmail.com>
2014-06-01 17:29:18 +0200

    Adding default redirect to map page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,33 @@
 'use strict';
 
 var biolabsApp = angular
-  .module('biolabsApp', ["leaflet-directive", "ui.router"]);
+  .module('biolabsApp', ['leaflet-directive', 'ui.router']);
 
-biolabsApp.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /map
-  $urlRouterProvider.otherwise("/map");
+biolabsApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+   // For any unmatched url, redirect to /map
+  $urlRouterProvider.otherwise('/map');
   //
   // Now set up the states
   $stateProvider
     .state('map', {
-      url: "/map",
-      templateUrl: "partials/map.html",
-      controller: "mapController"
+      url: '/map',
+      templateUrl: 'partials/map.html',
+      controller: 'mapController'
     })
     .state('add-lab', {
-      url: "/add-lab",
-      templateUrl: "partials/add_lab.html",
-      controller: "addLabController"
+      url: '/add-lab',
+      templateUrl: 'partials/add_lab.html',
+      controller: 'addLabController'
     })
     .state('contact', {
-      url: "/contact",
-        templateUrl: "partials/contact.html",
-        controller: "contactController"
+      url: '/contact',
+        templateUrl: 'partials/contact.html',
+        controller: 'contactController'
       });
 
+    $locationProvider.html5Mode(true);
+
+})
+.run(function($state) {
+   $state.transitionTo('map');
 });
