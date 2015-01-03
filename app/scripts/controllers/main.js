afefec158597599d9d6f0e02c0235cf68963d631
Natsu- <romain.garrigues.cs@gmail.com>
2015-01-03 15:35:42 +0000

    Map is now centered on markers bounding box

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,17 @@
 
 var biolabsApp = angular.module('biolabsApp'); 
 
-biolabsApp.controller('mapController', function ($scope, GeoJsonService, markers) {
+biolabsApp.controller('mapController', function ($scope, GeoJsonService, markers, leafletData) {
     $scope.markers = markers;
+
+    leafletData.getMap().then(function(map) {
+      var lmarkers = [];
+      angular.forEach(markers, function(marker) {
+        lmarkers.push(new L.marker([marker.lat, marker.lng]));
+      });
+      var group = new L.featureGroup(lmarkers);
+      map.fitBounds(group.getBounds());
+    });
 });
 
 
