e199fd4542b8108e74f65870f1e49c4efb409505
romgar <romain.garrigues.cs@gmail.com>
2015-09-06 21:16:55 +0100

    Fix #2 by managing form errors in add lab form + validation message if data is correct

--- app/scripts/controllers/blAddLabController.js
+++ app/scripts/controllers/blAddLabController.js
@@ -1,34 +1,30 @@
 
 biolabsApp.controller('blAddLabController', function ($state, $scope, $resource, $http, settings, ngDialog, $alert, blFlash) {
 
-    $scope.lab = {};
-
     var dialog = ngDialog.open({
         template: 'partials/add_lab.html',
         controller: ['$scope', function ($scope) {
+            $scope.errors = {};
             $scope.save = function (lab) {
                 var LabsResource = $resource(settings.API_ENDPOINT);
 
                 var dataToSave = new LabsResource(lab);
                 dataToSave.$save(function (data, headers) {
-                        if (data.success && data.success == true) {
-                            console.log("Data Sent Successfully!");
-                        }
-                        else {
-                            console.log("ERROR: " + JSON.stringify(data));
-                        }
+                        blFlash.setMessage('Thanks for your contribution !');
+                        $scope.closeThisDialog();
                     }
-                );
-                console.log(lab);
-                blFlash.setMessage('Thanks for your contribution !');
-                $scope.closeThisDialog();
+                , function(response) {
+                        var errors = response['data'];
+                        angular.forEach(errors, function(value, key) {
+                          $scope.errors[key] = value[0];
+                        });
+                    });
             };
         }]
     });
 
     dialog.closePromise.then(function (data) {
         $state.transitionTo('map');
-
     });
 
 });
