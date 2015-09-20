1c37d9741f4d307e32a1937adcb79356c2e9dc9e
romgar <romain.garrigues.cs@gmail.com>
2015-09-20 11:15:14 +0000

    Updating biolabs website

--- scripts/data/blLastLabController.js
+++ scripts/data/blLastLabController.js
@@ -2,7 +2,7 @@
 biolabsApp.controller('blLastLabController', function($scope, GeoJsonService) {
 
     GeoJsonService.getLabs().then(function(labs) {
-        $scope.labs = labs;
+        $scope.labs = labs.slice(0, 10);
     });
 
 });
