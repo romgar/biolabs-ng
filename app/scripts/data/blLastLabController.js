c766f6b8fdb7b2e0f6963f28610585545b41c568
romgar <romain.garrigues.cs@gmail.com>
2015-09-24 23:28:19 +0100

    Fix #12 : Only loading labs on map 'root' state, not every time we need it.

--- app/scripts/data/blLastLabController.js
+++ app/scripts/data/blLastLabController.js
@@ -1,8 +1,6 @@
 
 biolabsApp.controller('blLastLabController', function($scope, GeoJsonService) {
 
-    GeoJsonService.getLabs().then(function(labs) {
-        $scope.labs = labs.slice(0, 10);
-    });
+    $scope.labs = $scope.labs.slice(0, 10);
 
 });
